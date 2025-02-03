38bb32de1d9d000e4794e30e6e9b7ab604e8b910
DJ1211 <dan.jupe@yahoo.co.uk>
2025-02-03 12:18:10 +0000

    added connecting lines

--- cell.py
+++ cell.py
@@ -13,6 +13,8 @@ class Cell:
         self._win = win
 
     def draw(self, left_x, top_y, right_x, bottom_y):
+        if self._win is None:
+            return
         self._left_x = left_x
         self._right_x = right_x
         self._top_y = top_y
@@ -33,3 +35,19 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(left_x, bottom_y), Point(right_x, bottom_y))
             self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+        center_x, center_y = self._calculate_center()
+        to_cell_center_x, to_cell_center_y = to_cell._calculate_center()
+        
+        line = Line(Point(center_x, center_y), Point(to_cell_center_x, to_cell_center_y))
+        if undo:
+            self._win.draw_line(line, "gray")
+        else:
+            self._win.draw_line(line, "red")
+    
+    def _calculate_center(self):
+        center_x = (self._left_x + self._right_x) / 2
+        center_y = (self._top_y + self._bottom_y) / 2
+
+        return center_x, center_y
